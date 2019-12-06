fc7b10c9f1a4d84e5a3bd4b779d1a392b9200d65
Angelo Poole <angelo.poole@outlook.com>
2019-12-06 13:17:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,12 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  each_with_index.sort(array)
+  array.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
+end
+
+roll_call_dwarves(dwarves)
+
 end
 
 def summon_captain_planet# code an argument here
